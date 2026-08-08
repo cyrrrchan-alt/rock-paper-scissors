55f21c0a71afb29ab979af9300612100c65ddb63
cyrrrchan-alt <crystalchan110@gmail.com>
2026-08-08 15:34:16 -0700

    Add CPU randomly choosing rock, paper, or scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+// Randomly generate rock, paper, or scissors
+
+function getComputerChoice() {
+    const randomNum = Math.floor(Math.random() * 3);
+    let cpuChoice = "";
+
+    switch(randomNum) {
+        case 0:
+            // Rock
+            cpuChoice = "Rock";
+            break;
+        case 1:
+            // Paper
+            cpuChoice = "Paper";
+            break;
+        case 2:
+            // Scissors
+            cpuChoice = "Scissors";
+            break;
+    }
+
+    return cpuChoice;
+}
+
+console.log(getComputerChoice());
