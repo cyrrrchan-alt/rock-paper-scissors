53e7b6062b9907dec6bfec94b0539eb32d4be276
cyrrrchan-alt <crystalchan110@gmail.com>
2026-08-08 16:10:09 -0700

    Add win/lose/tie conditions

--- javascript.js
+++ javascript.js
@@ -5,19 +5,20 @@ let computerScore = 0;
 // Randomly generate rock, paper, or scissors
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3);
+    let cpuChoice = "";
 
     switch(randomNum) {
         case 0:
             // Rock
-            let cpuChoice = "rock";
+            cpuChoice = "rock";
             break;
         case 1:
             // Paper
-            let cpuChoice = "paper";
+            cpuChoice = "paper";
             break;
         case 2:
             // Scissors
-            let cpuChoice = "scissors";
+            cpuChoice = "scissors";
             break;
     }
 
@@ -33,8 +34,29 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-
+    // Tied 
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    }
+    // Player win conditions
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")) {
+        console.log("You win! " + humanChoice + " beats " + computerChoice + "!");
+        humanScore++;
+    }
+    // Computer win conditions
+    else if (
+        (computerChoice === "rock" && humanChoice === "scissors") ||
+        (computerChoice === "paper" && humanChoice === "rock") ||
+        (computerChoice === "scissors" && humanChoice === "paper")) {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice + "!");
+        computerScore++;
+    }
 }
 
-console.log(getHumanChoice());
-//console.log(getComputerChoice());
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
